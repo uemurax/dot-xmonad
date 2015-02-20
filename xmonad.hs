567adac02ac595697ecbf792de759b2e4ae728c1
UEMURA Taichi <t.uemura00@gmail.com>
2015-02-20 21:07:58 +0900

    show xmonad's status in xmobar

--- xmonad.hs
+++ xmonad.hs
@@ -1,16 +1,22 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
 myMask = modMask defaultConfig
 
 main = do
-  xmobar <- spawnPipe "xmobar"
+  xmproc <- spawnPipe "xmobar"
   xmonad $ defaultConfig
     { terminal = "urxvt"
     , manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , logHook = dynamicLogWithPP xmobarPP
+                  { ppOutput = hPutStrLn xmproc
+                  , ppTitle = xmobarColor "green" "" . shorten 50
+                  }
     , startupHook = spawn "xscreensaver -no-splash"
     } `additionalKeys`
     [ ((myMask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
