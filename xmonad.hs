f9096f77e0cb45c95d978464fc4511d2957ae9d5
UEMURA Taichi <t.uemura00@gmail.com>
2015-02-20 18:23:25 +0900

    introduced xmobar

--- xmonad.hs
+++ xmonad.hs
@@ -1,11 +1,16 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 
 myMask = modMask defaultConfig
 
 main = do
+  xmobar <- spawnPipe "xmobar"
   xmonad $ defaultConfig
     { terminal = "urxvt"
+    , manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts $ layoutHook defaultConfig
     , startupHook = spawn "xscreensaver -no-splash"
     } `additionalKeys`
     [ ((myMask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
