dd1763ea92bd6c1f2d10132592e5240f2bdced86
UEMURA Taichi <t.uemura00@gmail.com>
2015-02-25 16:18:33 +0900

    added shortcuts on working spaces

--- xmonad.hs
+++ xmonad.hs
@@ -1,16 +1,19 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import qualified XMonad.StackSet as W
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeysP)
 import System.IO
 
 myMask = modMask defaultConfig
+myWorkspaces = map (\x -> [x]) ['a'..'z']
 
 main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ defaultConfig
     { terminal = "urxvt"
+    , workspaces = myWorkspaces
     , manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts $ layoutHook defaultConfig
     , logHook = dynamicLogWithPP xmobarPP
@@ -18,7 +21,13 @@ main = do
                   , ppTitle = xmobarColor "green" "" . shorten 50
                   }
     , startupHook = spawn "xscreensaver -no-splash"
-    } `additionalKeysP`
+    } `additionalKeysP` (
     [ ("M-S-z" , spawn "xscreensaver-command -lock")
+    ] ++
+    [ ("M-u " ++ otherModMasks ++ tag, action tag)
+      | tag <- myWorkspaces
+      , (otherModMasks, action) <- [ ("", windows . W.greedyView)
+                                   , ("S-", windows . W.shift)]
     ]
+    )
 
