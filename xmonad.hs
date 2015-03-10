af690c1cd5738c15b5c25a058f74f6f0b42055ab
UEMURA Taichi <t.uemura00@gmail.com>
2015-03-10 12:30:00 +0900

    added "swap with hints" function

--- xmonad.hs
+++ xmonad.hs
@@ -19,10 +19,38 @@ myXPConfig = defaultXPConfig
   }
 hintChar = map (\x -> [x]) "jfnvuthgybkdmciroelspwaqz"
 
--- Hint functions
+-- $Hint functions
 swapMasterWith :: (Eq s, Eq a, Eq i) =>
   a -> W.StackSet i l a s sd -> W.StackSet i l a s sd
 swapMasterWith a = W.swapMaster . W.focusWindow a
+
+-- | swap the current focused window and the specified window and focus the specified window
+swapWith :: (Eq a) =>
+  a -> W.StackSet i l a s sd -> W.StackSet i l a s sd
+swapWith = W.modify' . swapWith'
+
+swapWith' :: (Eq a) =>
+  a -> W.Stack a -> W.Stack a
+swapWith' a stk@(W.Stack t ls rs) =
+  if a == t then stk
+  else case findWindow a ls rs of
+    Nothing -> stk
+    Just (Left (ll, lr)) -> W.Stack t ll (lr ++ [a] ++ rs)
+    Just (Right (rl, rr)) -> W.Stack t (rr ++ [a] ++ ls) rl
+
+findWindow :: (Eq a) => a -> [a] -> [a] -> Maybe (Either ([a], [a]) ([a], [a]))
+findWindow a ls rs = case findZ a (ls, []) of
+  Nothing -> case findZ a (rs, []) of
+    Nothing -> Nothing
+    Just r -> Just (Right r)
+  Just l -> Just (Left l)
+
+findZ :: (Eq a) => a -> ([a], [a]) -> Maybe ([a], [a])
+findZ a ([], _) = Nothing
+findZ a (x:xs, ys) =
+  if a == x then Just (xs, ys)
+  else findZ a (xs, x:ys)
+
 
 main = do
   xmproc <- spawnPipe "xmobar"
@@ -56,7 +84,8 @@ main = do
     ] ++
     [ ("M-f", runHints focus hintChar) ] ++
     [ ("M-; " ++ key, runHints action hintChar)
-    | (key, action) <- [ ("f", focus), ("c", killWindow), ("m", windows . swapMasterWith) ] ++
+    | (key, action) <- [ ("f", focus), ("c", killWindow)
+        , ("m", windows . swapMasterWith), ("s", windows . swapWith) ] ++
         [ ("u " ++ otherModMasks ++ tag, action tag)
         | tag <- myWorkspaces
         , (otherModMasks, action) <- [ ("S-", (\t -> windows . W.shiftWin t))
