537c0be817157313f2e9739fbd50c1d50c6ee09b
UEMURA Taichi <t.uemura00@gmail.com>
2015-03-24 18:43:52 +0900

    test2

--- lib/XMonad/Prompt/Hints.hs
+++ lib/XMonad/Prompt/Hints.hs
@@ -3,19 +3,24 @@ module XMonad.Prompt.Hints
   , defaultHConfig
   ) where
 
-import qualified Data.Map as M
+import Control.Concurrent
 
 import XMonad
 import XMonad.Prompt
+import XMonad.Util.Font
 import XMonad.Util.Hints
 import XMonad.Util.XUtils
 
 data HintConfig = HintConfig
   { hintChar :: String
+  , hintFont :: String
+  , hintTitleLen :: Int
   }
 
 defaultHConfig = HintConfig
   { hintChar = "jfnvuthgbymridkcoelspwaqz"
+  , hintFont = "fixed"
+  , hintTitleLen = -1
   }
 
 data HintsPrompt = Focus
@@ -29,35 +34,55 @@ hintsPromptFocus = doPrompt Focus
 
 doPrompt :: HintsPrompt -> HintConfig -> XPConfig -> X ()
 doPrompt t h c = do
+  dpy <- asks display
+  xmf <- initXMF $ hintFont h
   wm <- windowMap $ hintChar h
-  ws <- createHints wm
+  hs <- createHints xmf wm
+  let ws = map (\(x, y) -> y) hs
   showWindows ws
+  drawHints dpy xmf hs
   a <- case t of
     Focus -> return $ focusAction wm
   mkXPrompt t c (\_ -> return []) a
   deleteWindows ws
+  releaseXMF xmf
 
-focusAction :: M.Map String Window -> String -> X ()
+focusAction :: [(String, Window)] -> String -> X ()
 focusAction wm s = do
-  let mw = M.lookup s wm
+  let mw = lookup s wm
   case mw of
     Nothing -> return ()
     Just w -> focus w
 
-createHints :: M.Map String Window -> X ([Window])
-createHints wm = createHints' $ M.toList wm
-
-createHints' :: [(String, Window)] -> X ([Window])
-createHints' [] = return []
-createHints' (w:ws) = do
-  p <- createHint w
-  ps <- createHints' ws
+createHints :: XMonadFont -> [(String, Window)] -> X ([(String, Window)])
+createHints xmf [] = return []
+createHints xmf (w:ws) = do
+  p <- createHint xmf w
+  ps <- createHints xmf ws
   return (p:ps)
 
-createHint :: (String, Window) -> X Window
-createHint (str, win) = do
+createHint :: XMonadFont -> (String, Window) -> X (String, Window)
+createHint xmf (str, win) = do
   dpy <- asks display
+  width <- textWidthXMF dpy xmf str
+  (ascent, descent) <- textExtentsXMF xmf str
   (rootw, x, y, w, h, b, d) <- io $ getGeometry dpy win
-  ret <- createNewWindow (Rectangle x y 10 10) Nothing "" False
-  return ret
+  let x' = x + fi ((fi w - width) `div` 2)
+      height = ascent + descent
+      y' = y + fi ((fi h - height) `div` 2)
+  ret <- createNewWindow (Rectangle x' y' (fi width) (fi height)) Nothing "" False
+  return (str, ret)
+
+drawHint :: Display -> XMonadFont -> (String, Window) -> X ()
+drawHint dpy xmf (str, win) = do
+  gc <- io $ createGC dpy win
+  (ascent, descent) <- textExtentsXMF xmf str 
+  printStringXMF dpy win xmf gc "black" "yellow" 0 ascent str
+  io $ freeGC dpy gc
+
+drawHints :: Display -> XMonadFont -> [(String, Window)] -> X ()
+drawHints dpy xmf [] = return ()
+drawHints dpy xmf (w:ws) = do
+  drawHint dpy xmf w
+  drawHints dpy xmf ws
 
--- lib/XMonad/Util/Hints.hs
+++ lib/XMonad/Util/Hints.hs
@@ -2,8 +2,6 @@ module XMonad.Util.Hints
   ( windowMap
   ) where
 
-import qualified Data.Map as M
-
 import XMonad
 import qualified XMonad.StackSet as W
 
@@ -13,12 +11,12 @@ diag l = map (\x -> [x]) l ++ [x:xs | xs <- diag l, x <- l]
 mkHintStr :: [a] -> [[a]]
 mkHintStr l = map reverse $ diag l
 
-windowMap' :: W.StackSet i l a s sd -> [String] -> M.Map String a
-windowMap' stk ss = M.fromList . zip ss $ W.index stk
+windowMap' :: W.StackSet i l a s sd -> [String] -> [(String, a)]
+windowMap' stk ss = zip ss $ W.index stk
 
-windowMap'' :: [String] -> X (M.Map String Window)
+windowMap'' :: [String] -> X ([(String, Window)])
 windowMap'' ss = withWindowSet (\ws -> return (windowMap' ws ss))
 
-windowMap :: String -> X (M.Map String Window)
+windowMap :: String -> X ([(String, Window)])
 windowMap s = windowMap'' $ mkHintStr s
 
