9380d553c1cdb4e2c1ceb01cb8bb254c67aa1c11
UEMURA Taichi <t.uemura00@gmail.com>
2015-09-01 09:24:17 +0900

    add offset for circle layout

--- lib/XMonad/Layout/CircleEX.hs
+++ lib/XMonad/Layout/CircleEX.hs
@@ -13,22 +13,26 @@ import qualified XMonad.StackSet as W
 data CircleEX a = CircleEX {
   circleNMaster :: !Int,
   circleRatioIncrement :: !Rational,
-  circleRatio :: !Rational }
+  circleRatio :: !Rational,
+  circleOffset :: !Rational }
                   deriving (Show, Read)
 
 instance LayoutClass CircleEX Window where
-  doLayout (CircleEX nmaster _ frac) r s = do
+  doLayout (CircleEX nmaster _ frac offset) r s = do
     layout <- raiseFocus $ zip ws rs
     return (layout, Nothing) where
       ws = W.integrate s
-      rs = circleLayout frac r nmaster (length ws)
+      rs = circleLayout frac offset r nmaster (length ws)
 
-  pureMessage (CircleEX nmaster delta frac) m =
+  pureMessage c m =
     msum [ fmap resize (fromMessage m),
            fmap incmastern (fromMessage m)] where
-      resize Shrink = CircleEX nmaster delta (max 0 $ frac - delta)
-      resize Expand = CircleEX nmaster delta (min 1 $ frac + delta)
-      incmastern (IncMasterN d) = CircleEX (max 0 (nmaster + d)) delta frac
+      nmaster = circleNMaster c
+      delta = circleRatioIncrement c
+      frac = circleRatio c
+      resize Shrink = c { circleRatio = max 0 $ frac - delta }
+      resize Expand = c { circleRatio = min 1 $ frac + delta }
+      incmastern (IncMasterN d) = c { circleNMaster = max 0 (nmaster + d) }
 
   description _ = "CircleEX"
 
@@ -39,13 +43,13 @@ raiseFocus xs = do
             Just x -> x : delete x xs
             Nothing -> xs
 
-circleLayout :: Rational -> Rectangle -> Int -> Int -> [Rectangle]
-circleLayout frac r nmaster n = center ++ rest where
+circleLayout :: Rational -> Rational -> Rectangle -> Int -> Int -> [Rectangle]
+circleLayout frac offset r nmaster n = center ++ rest where
   nmaster' = if nmaster < 0 then 0
              else if nmaster > n then n
                   else nmaster
   center = centerRect frac r nmaster'
-  rest = satellite (1 - frac) r (n - nmaster')
+  rest = satellite (1 - frac) offset r (n - nmaster')
 
 centerRect :: Rational -> Rectangle -> Int -> [Rectangle]
 centerRect _ _ n | n < 1 = []
@@ -56,9 +60,9 @@ centerRect frac (Rectangle x y w h) n =
     w' = fromIntegral w * frac
     h' = fromIntegral h * frac
 
-satellite :: Rational -> Rectangle -> Int -> [Rectangle]
-satellite frac (Rectangle x y w h) n =
-  map f [0, 2 * pi / fromIntegral n ..] where
+satellite :: Rational -> Rational -> Rectangle -> Int -> [Rectangle]
+satellite frac offset (Rectangle x y w h) n =
+  map f [ x + 2 * pi * fromRational offset | x <- [0, 2 * pi / fromIntegral n ..]] where
     f t = Rectangle x' y' (floor w') (floor h') where
       x' = x + round ((fromIntegral w - w') * (1.0 + cos t) / 2)
       y' = y + round ((fromIntegral h - h') * (1.0 + sin t) / 2)
