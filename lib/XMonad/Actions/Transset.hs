4364778f1a71b8ac1976e7744d328f13ad62726a
Taichi Uemura <t.uemura00@gmail.com>
2017-01-14 21:04:36 +0900

    set default opacity

--- lib/XMonad/Actions/Transset.hs
+++ lib/XMonad/Actions/Transset.hs
@@ -13,14 +13,15 @@ runTransset :: TranssetConfig -> Transset -> Window -> X ()
 runTransset c t w = spawn $ "transset -i " ++ show w ++ " " ++ f t
   where f Inc = "--inc " ++ show (transIncrement c)
         f Dec = "--dec " ++ show (transIncrement c)
-        f Toggle = "--toggle"
+        f Toggle = "--toggle " ++ show (transDefault c)
 
 data TranssetConfig = TranssetConfig
   { transIncrement :: Float
+  , transDefault :: Float
   }
 
 instance Default TranssetConfig where
   def = TranssetConfig
         { transIncrement = 0.05
+        , transDefault = 0.9
         }
-
