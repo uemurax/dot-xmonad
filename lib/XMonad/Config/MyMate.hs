5334e9dc8f87093c11750adc00c61c590775ac69
Taichi Uemura <t.uemura00@gmail.com>
2020-02-29 11:49:51 +0100

    update mate config

--- lib/XMonad/Config/MyMate.hs
+++ lib/XMonad/Config/MyMate.hs
@@ -4,8 +4,11 @@ module XMonad.Config.MyMate (
 
 import XMonad
 import XMonad.Config.Mate
+import XMonad.Util.EZConfig
 
 myMate cfg =
   cfg { terminal = "mate-terminal"
       , startupHook = mateRegister >> startupHook cfg
-      }
+      } `additionalKeysP`
+  [ ("M-S-q", spawn "mate-session-save --logout-dialog")
+  ]
